5cd9bf7e83142f2c29e19e373c2df4626033dccb
Ben Fediuck <benfe@mta.ac.il>
2026-06-07 11:03:01 +0300

    conflic resolved

---
+++ Ex04.Menus.Events/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Ex04.Menus.Events
+{
+    public class MainMenu : MenuItem 
+    {
+        public MainMenu(string i_MenuTitle) : base(i_MenuTitle)
+        {
+
+        }
+        protected override void printMenuMessage()
+        {
+            Console.WriteLine($"Please enter your choice (1-{r_SubMenus.Count} | or 0 to Exit)");
+        }
+        
+    }
+}
