404b6996c42ad4f2f18411f97cedeae741052ad3
Ben Fediuck <benfe@mta.ac.il>
2026-06-07 10:54:34 +0300

    dlltest addition

---
+++ Ex04.Menus.Test/MenuDllTester.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Ex04.Menus.Test
+{
+    internal class MenuDllTester
+    {
+        public Ex04.Menus.Events.MainMenu EventsMenuTester()
+        {
+            Ex04.Menus.Events.MainMenu eventsMainMenu = new Ex04.Menus.Events.MainMenu("Main Menu");
+            Ex04.Menus.Events.MenuItem eventsShowTimeMenu = new Ex04.Menus.Events.MenuItem("Show Current Date/Time");
+            Ex04.Menus.Events.MenuItem eventsShowVersionAndCapitalsMenu = new Ex04.Menus.Events.MenuItem("Version and Capitals");
+            eventsMainMenu.AddSubMenu(eventsShowTimeMenu);
+            eventsMainMenu.AddSubMenu(eventsShowVersionAndCapitalsMenu);
+            Ex04.Menus.Events.ActionMenu ShowCurrentTime = new Ex04.Menus.Events.ActionMenu("Show Current Time");
+            Ex04.Menus.Events.ActionMenu ShowCurrentDate = new Ex04.Menus.Events.ActionMenu("Show Current Date");
+            eventsShowTimeMenu.AddSubMenu(ShowCurrentTime);
+            eventsShowTimeMenu.AddSubMenu(ShowCurrentDate);
+            Ex04.Menus.Events.ActionMenu ShowVersion = new Ex04.Menus.Events.ActionMenu("Show Version");
+            Ex04.Menus.Events.ActionMenu CountCapitals = new Ex04.Menus.Events.ActionMenu("Count Capitals");
+            eventsShowVersionAndCapitalsMenu.AddSubMenu(ShowVersion);
+            eventsShowVersionAndCapitalsMenu.AddSubMenu(CountCapitals);
+            eventsMainMenu.Show();
+
+            return eventsMainMenu;
+        }
+    }
+}
